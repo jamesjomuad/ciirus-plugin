6f0ea64f131f5653d4b3e71da47ff279ed63b791
james <st.james.jomuad@gmail.com>
2019-01-18 14:43:28 +0800

    Get Properties

--- assets/js/plugins.js
+++ assets/js/plugins.js
@@ -73,6 +73,20 @@
         return (window.location.href.indexOf(page) > -1);
     }
 
+    w.getProperties = (function(){
+        Properties = null;
+        var getProperties = function(){
+            $.get('/JumpToProperty16001.aspx')
+            .then(function(res){
+                var start   = res.indexOf("= [ {")+1;
+                var end     = res.indexOf("}];")+2;
+                Properties  = eval(res.substring(start,end));
+            });
+            return Properties;
+        }; getProperties();
+        return getProperties;
+    })()
+
     return w;
 })(window,jQuery);
 
